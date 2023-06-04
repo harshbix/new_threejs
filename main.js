ce1708d6893b198801d39a975f3d8c13352e63b1
harshbix <kidabixson@gmail.com>
2023-06-04 16:13:05 +0300

    added gsap effects

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import "./style.css";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { gsap } from "gsap";
 // make a scene
 const scene = new THREE.Scene();
 
@@ -59,3 +60,10 @@ const loop = () => {
   window.requestAnimationFrame(loop);
 };
 loop();
+
+//timeline magiccccccc
+
+const tl = gsap.timeline({ defaults: { duration: 1.2 } });
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
